6abe1f5dc82227c2b9aca15c09f515b91e714f7d
parfenyukmisha <24a_pmya@ztu.edu.ua>
2025-02-17 09:50:20 +0200

    додано інформацію

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn_love_wishes").addEventListener('click', function() {
+    //console.log(document.getElementById("btn_love_wishes"))
+    document.getElementById('love_wishes').innerText='Нехай кожен приносить радість і тепло!'
+})
